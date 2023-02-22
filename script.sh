b95230595bd49c062152559eb557765a59a3e47e
Urs Braun <urs-benedict.braun@st.ovgu.de>
2023-02-22 08:38:25 +0100

    add speicherplatz und mount points

--- script.sh
+++ script.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
- 
-
 # Autor: Urs-Benedict Braun <urs-benedict.braun@dataport.de>
 
  
-
+# kleiner Banner in ASCII Art
 echo " _______  _______         ___   __    _  _______  _______ "
 echo "|       ||       |       |   | |  |  | ||       ||       |"
 echo "|   _   ||  _____| ____  |   | |   |_| ||    ___||   _   |"
@@ -14,11 +12,23 @@ echo "|  |_|  ||_____  |       |   | |  _    ||    ___||  |_|  |"
 echo "|       | _____| |       |   | | | |   ||   |    |       |"
 echo "|_______||_______|       |___| |_|  |__||___|    |_______|"
 echo ""
- 
+
+# Hier sollen die Informationen vom Betriebsystem sein
 echo "* OS"
-cat /etc/os-release | grep "NAME" | head -n2 | tail -n1 | awk -F'=' '{print "* The operating system is " $2}'
+cat /etc/os-release | grep "NAME" | head -n2 | tail -n1 | awk -F'=' '{print "  * The operating system is " $2}'
 
  
 echo ""
+
+# Hier sollen die Informationen vom Arbeitsspeicher sein
+# Diese Informationen erhalten wir von free -h
+# -h bedeutet "Human readable", also was die Station will
 echo "* RAM"
 free -h | tail -n2 | awk '{print "  * The ram usage of " $1 " is " $3 " from total of " $2}'
+
+echo ""
+
+# Hier sollen die Informationen vom Speicher sei
+df -h | head -n1 |awk '{print "* " $0}'
+ df -h | sed -e '1d' |awk '{print "  * " $0}'
+
