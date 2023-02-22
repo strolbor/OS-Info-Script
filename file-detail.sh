425927f2bc30611769276bc629a30ba42479260a
Urs Braun <urs-benedict.braun@st.ovgu.de>
2023-02-22 08:59:12 +0100

    english

--- file-detail.sh
+++ file-detail.sh
@@ -1,7 +1,9 @@
 echo "* Filesystem"
 echo "  * Mountpoints"
+# $6 sind die Mountpoints
 df -h | sed -e '1d' | awk '{print "    * " $6}'
 
 
 echo "  * Storage usage"
-df -h | sed -e '1d' |awk '{print "    * " $1 " nutzt grade " $3 " von insgesamt " $2}'
+# Device uses just 0M out of a total of 1M
+df -h | sed -e '1d' |awk '{print "    * " $1 " uses just " $3 " out of a total of " $2}'
