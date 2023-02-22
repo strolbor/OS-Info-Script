7bd797ad56ecc006b82972bfebf27ff126b5ab40
Urs Braun <urs-benedict.braun@st.ovgu.de>
2023-02-22 08:28:33 +0100

    in english

--- script.sh
+++ script.sh
@@ -13,17 +13,12 @@ echo "|  | |  || |_____ |____| |   | |       ||   |___ |  | |  |"
 echo "|  |_|  ||_____  |       |   | |  _    ||    ___||  |_|  |"
 echo "|       | _____| |       |   | | | |   ||   |    |       |"
 echo "|_______||_______|       |___| |_|  |__||___|    |_______|"
-
+echo ""
  
-
-echo "* OS-Infos"
-
-cat /etc/os-release | grep "NAME" | head -n2 | tail -n1 | awk -F'=' '{print "  * " $2}'
+echo "* OS"
+cat /etc/os-release | grep "NAME" | head -n2 | tail -n1 | awk -F'=' '{print "* The operating system is " $2}'
 
  
 echo ""
-echo "* Ram"
-
-echo "Die Speicherauslastung ist von:"
-
-free -h | tail -n2 | awk '{print "* " $1 " nutzt grade " $3 " von insgesamt: " $2}'
+echo "* RAM"
+free -h | tail -n2 | awk '{print "  * The ram usage of " $1 " is " $3 " from total of " $2}'
