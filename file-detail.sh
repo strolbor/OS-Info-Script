b07fb6c45945e1de92592aa4afe8901d2cd35293
Urs Braun <urs-benedict.braun@st.ovgu.de>
2023-02-22 08:52:57 +0100

    korrektur syntax

--- file-detail.sh
+++ file-detail.sh
@@ -1,7 +1,7 @@
 echo "* Filesystem"
 echo "  * Mountpoints"
-df -h | awk '{print "  * " $6}'
+df -h | sed -e '1d' | awk '{print "    * " $6}'
 
 
 echo "  * Storage usage"
-df -h | awk '{print "    * "$1 "nutzt grade $6 von insgesamt" $2}'
+df -h | awk '{print "    * " $1 "nutzt grade " $6 " von insgesamt" $2}'
