9a3167c9e5df356a84acaf1ec3bd3b9dae0534b3
Urs Braun <github@ursb.de>
2023-02-22 13:09:29 +0100

    Prozent cpu load

--- script.sh
+++ script.sh
@@ -28,13 +28,21 @@ lscpu | grep "CPU(s)" | head -n1 | awk '{print "  * CPU cores: " $2}'
 uptime -p | sed 's/up//g' |awk '{print "  * Uptime:"$0}'
 
 echo "  * CPU-load"
-top -b -n 1 |head -n1 | awk '{print "    * over the last 1 minute: "$10 }' | sed -e 's/ $//g' | sed -e 's/,$//g' 
-top -b -n 1 |head -n1 | awk '{print "    * over the last 5 minute: "$11 }' | sed -e 's/ $//g' | sed -e 's/,$//g'
-top -b -n 1 |head -n1 | awk '{print "    * over the last 15 minute: "$12 }'
+top -b -n 1 -i | head -n1 > top.tmp
+cat top.tmp | awk '{print "    * over the last 1 minute: "$10 }' top.tmp | sed -e 's/ $//g' | sed -e 's/,$//g' | sed -e 's/.$//.'
+cat top.tmp | awk '{print "    * over the last 5 minute: "$11 }' | sed -e 's/ $//g' | sed -e 's/,$//g'
+cat top.tmp | awk '{print "    * over the last 15 minute: "$12 }'
 
+#in Prozent
 
-
-
+cat top.tmp |head -n1 | awk '{print $10 }' | sed -e 's/ $//g' | sed -e 's/,$//g'  | sed  's/,/./g' | awk '{print "    * over the last 15 minute: " $0*100 "%"}'
+# 1. sed leerzeichen entfernen
+# 2. sed , entfernen
+# 3. sed , durch . ersetzen
+# dann multiplizieren für prozent
+cat top.tmp | awk '{print $11 }' | sed -e 's/ $//g' | sed -e 's/,$//g'  | sed  's/,/./g' | awk '{print "    * over the last 15 minute: " $0*100 "%"}'
+cat top.tmp | awk '{print $12 }' | sed -e 's/ $//g' | sed -e 's/,$//g'  | sed  's/,/./g' | awk '{print "    * over the last 15 minute: " $0*100 "%"}'
+rm -f top.tmp
 
 echo ""
 
